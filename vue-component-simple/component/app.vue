d04d15672c95e48fae2e8d6702f9d4423c0d4468
chace0120@126.com <chace0120@126.com>
2018-02-13 17:28:11 +0800

    添加【标题组件】的demo

---
+++ vue-component-simple/component/app.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<v-title title="Vue标题组件"></v-title>
+	</div>
+</template>
+
+<script>
+	import vTitle from './title.vue';
+
+	export default {
+		components: {
+			vTitle
+		}
+	}
+</script>
